ba4d250b45ae4d53ed7f8fe4d9e635979abd284f
gregory.tall <gregory.tall@ext.cdiscount.com>
2017-04-20 09:35:05 +0200

    clean

--- src/CdiscountMarketPlaceApi_WebApp/Models/Autentication.cs
+++ src/CdiscountMarketPlaceApi_WebApp/Models/Autentication.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using System.IO;
 using System.Text;
-using System.Net;
-using Microsoft.Extensions.DependencyInjection;
 using System.Net.Http;
 using System.Runtime.Serialization;
 using CdscntMkpApiReference_Prod;
@@ -18,12 +13,8 @@ namespace CdiscountMarketPlaceApi_WebApp.Models
         public enum EnvironmentName
         {
             Production,
-            Preproduction/*,
-            Recette,
-            Dev,
-            Sandbox,
-            Local*/
-        }
+            Preproduction
+         }
         public string _Login { get; set; }
         public string _Password { get; set; }
         public string _Environment { get; set; }
@@ -31,14 +22,7 @@ namespace CdiscountMarketPlaceApi_WebApp.Models
         public static IConfigurationRoot Configuration { get; set; }
 
         public Autentication() {
-            /*var builder = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-           .AddJsonFile("appsettings.json");
-            Configuration = builder.Build();
-            */
-           
-            //  _MarketplaceAPIService = new CdscntMkpApiReference_Prod.MarketplaceAPIServiceClient();
-
+       
         }
 
     public string GetToken()
@@ -69,21 +53,11 @@ namespace CdiscountMarketPlaceApi_WebApp.Models
                 string encoded = Convert.ToBase64String(Encoding.UTF8.GetBytes(string.Format("{0}:{1}", _Login, _Password)));
 
                 var stsUri = new Uri(string.Format("{0}/?realm={1}", svcIssue, realm));
-                /*   var request = (HttpWebRequest)WebRequest.Create(stsUri);
-
-                   request.Headers["HttpRequestHeader"].Add("Authorization", string.Format("Basic={0}", encoded));
-                   request.Method = "GET";
-
-                   var response = await request.GetResponseAsync();
-                   var tokenStream = response.GetResponseStream();
-                  */
-                Stream  myttokenStreamoken;
+               Stream  myttokenStreamoken;
                 using (HttpClient client = new HttpClient())
                 {
                     client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", encoded);
-                    //.Add("Authorization", string.Format("Basic={0}", encoded));
                     var data =  client.GetAsync(stsUri).Result;
-                     //myttokenStreamoken = await data.Content.ReadAsStringAsync();
                     myttokenStreamoken =  data.Content.ReadAsStreamAsync().Result;
                    
                 }
